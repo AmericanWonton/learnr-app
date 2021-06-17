2a9c0ad602e5d07362c2c7578a2309cf2c2a5c15
AmericanWonton <jbkeller0303@gmail.com>
2021-06-16 19:01:01 -0500

    Added some buttons and funcitons to handle LearnR creation..now need to submit on the back end by clicking submit.

--- project/webapp/golang/testing-project/static/js/learnr.js
+++ project/webapp/golang/testing-project/static/js/learnr.js
@@ -1,6 +1,9 @@
 var learnrTags = 0; //This increases every time we click our buttons,(used mostly for naming)
 let learnrTagStrings = new Map(); //This contains all of our tags
 
+var learnrInformsCount = 0; //This increases every time we click our buttons,(used mostly for naming)
+let learnrInforms = new Map(); //This contains all our LearnRInforms
+
 //Add our listener events to window loading
 window.addEventListener('DOMContentLoaded', function(){
     var theLearnR = {
@@ -24,6 +27,10 @@ window.addEventListener('DOMContentLoaded', function(){
     var learnrorgs = document.getElementById("learnrorgs");
     var informtextPLearnOrg = document.getElementById("informtextPLearnOrg");
     var submitLearnR = document.getElementById("submitLearnR");
+
+    /* Intitially set this to disabled so User needs to input values
+    for this LearnR */
+    submitLearnR.disabled = true;
 
     /* Check the database for LearnR Name when the key is pressed! */
     learnrname.addEventListener('input', function(){
@@ -67,19 +74,19 @@ window.addEventListener('DOMContentLoaded', function(){
                 var item = xhr.responseText;
                 if (item == 'TooShort') {
                     informtextPLearnOrg.innerHTML = 'Please tell us why you want to make this LearnR Organization';
-                    submitLearnROrg.disabled = true;
+                    submitLearnR.disabled = true;
                 } else if (item == 'TooLong'){
                     informtextPLearnOrg.innerHTML = 'LearnR Organization about section must be under 400 characters';
-                    submitLearnROrg.disabled = true;
+                    submitLearnR.disabled = true;
                 } else if (item == 'ContainsLanguage'){
                     informtextPLearnOrg.innerHTML = 'This section contains innapropriate content; please contact our help center for more information.';
-                    submitLearnROrg.disabled = true;
+                    submitLearnR.disabled = true;
                 } else if (item == 'okay') {
                     informtextPLearnOrg.innerHTML = '';
-                    submitLearnROrg.disabled = false;
+                    submitLearnR.disabled = false;
                 } else {
                     informtextPLearnOrg.innerHTML = 'Error checking your LearnR Organiztion about section';
-                    submitLearnROrg.disabled = true;
+                    submitLearnR.disabled = true;
                 }
             }
         });
@@ -87,8 +94,25 @@ window.addEventListener('DOMContentLoaded', function(){
     });
 
     /* Changes our LearnR object to the organization ID when dropdown menu is selected */
-    learnrorgs.addEventListener('onchange', function(){
+    learnrorgs.addEventListener('change', function(){
         theLearnR.OrgID = Number(learnrorgs.value); //Adjust orgID value
+    });
+
+    /* Disables the 'timewaiting' obect based on whether or not
+    'timewait' is true or false */
+    timewait.addEventListener('change', function(){
+        var timewaiting = document.getElementById("timewaiting");
+
+        if (timewait.value === "true") {
+            timewaiting.disabled = false;
+        } else {
+            timewaiting.disabled = true;
+        }
+    });
+
+    /* Submit this LearnR for review! */
+    submitLearnR.addEventListener('click', function(){
+
     });
 
 });
@@ -136,4 +160,67 @@ function addLearnRTag(){
         console.log("Here is our key: " + key + " and here is our value: " + value);
     }
     learnrTags = learnrTags + 1; //Needed to interact with our map and other values
+}
+
+/* Called when '' is clicked. Create a new inform and add it to our 
+learnrInforms. These can also be deleted at any time */
+function addInform(){
+    /* Declare our variables for later use */
+    var textDesc = document.getElementById("textDesc");
+    var timewait = document.getElementById("timewait");
+    var timewaiting = document.getElementById("timewaiting");
+    var resultHolderInformDiv = document.getElementById("resultHolderInformDiv");
+
+    /* Create our LearnrInform object */
+    var learnrInform = {
+        ID: 0,
+        Name: "",
+        LearnRID: "",
+        LearnRName: "",
+        Order: 0,
+        TheInfo: String(textDesc.value),
+        ShouldWait: false,
+        WaitTime: 0,
+        DateCreated: "",
+        DateUpdated: ""
+    };
+    /* Add time wait if it is selected */
+    if (timewait.value != "false") {
+        learnrInform.ShouldWait = true;
+        learnrInform.WaitTime = Number(timewaiting.value);
+    } 
+
+    /* Create visual representation and add to our array */
+    /* Create our divs to append to resultHolderTagDiv */
+    var resultLearnrInformDiv = document.createElement("div");
+    resultLearnrInformDiv.setAttribute("id", "resultLearnrInformDiv" + learnrInformsCount.toString());
+    resultLearnrInformDiv.setAttribute("class", "resultLearnrInformDiv");
+    resultLearnrInformDiv.setAttribute("name", "resultLearnrInformDiv" + learnrInformsCount.toString());
+    
+    var resultInformP = document.createElement("p");
+    resultInformP.setAttribute("id", "resultInformP" + learnrInformsCount.toString());
+    resultInformP.setAttribute("class", "resultInformP");
+    resultInformP.setAttribute("name", "resultInformP" + learnrInformsCount.toString());
+    resultInformP.innerHTML = String(textDesc.value).substr(0,20) + "..." + "//ShouldWait: " +
+    timewait.value + "//TimeWait: " + timewaiting.value.toString();
+
+    /* Add first elements to each other */
+    resultLearnrInformDiv.appendChild(resultInformP);
+    /* Add the LearnRInform to our current map */
+    learnrInforms.set(Number(learnrInformsCount), String(textDesc.value));
+    /* add the appropriate function on click */
+    var thePosition = Number(learnrInformsCount); //Used for deleteing tags
+    resultLearnrInformDiv.addEventListener("click", function(){
+        //Remove from this position
+        learnrInforms.delete(thePosition);
+        resultLearnrInformDiv.remove();
+    });
+    /* Add to result holder div for display */
+    resultHolderInformDiv.appendChild(resultLearnrInformDiv);
+    /* Clear the text box after entry */
+    textDesc.value = "";
+    textDesc.innerHTML = "";
+    timewaiting.value = 0;
+
+    learnrInformsCount = learnrInformsCount + 1; //Increment this for future naming values
 }
