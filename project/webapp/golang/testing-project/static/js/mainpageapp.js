42b9d4f889d348d35dad6c0951d3ef6d9972b0ee
AmericanWonton <jbkeller0303@gmail.com>
2021-06-23 09:33:17 -0500

    Fixed bug where we couldn't get learnrs added properly. Moving onto visual touches with ANgular.:

--- project/webapp/golang/testing-project/static/js/mainpageapp.js
+++ project/webapp/golang/testing-project/static/js/mainpageapp.js
@@ -38,16 +38,36 @@ app.controller('myCtrl', function($scope) {
         DateCreated: "",
         DateUpdated: ""
     };
-    //LearnrSet
+    /* LearnrSet
+    Calls Ajax to get our Learnrs and put them into our jsLearnRArray */
     $scope.learnRSet = function() {
-        var testJSONVar = String(getLearnrsAjax());
-        $scope.SuccessMSG = JSON.parse(testJSONVar);
-        $scope.jsLearnRArray = $scope.SuccessMSG.TheDisplayLearnrs;
-        //$scope.jsLearnRArray = item;
-        console.log("Here is our jsLearnRArray: " + JSON.stringify($scope.jsLearnRArray) + "\n");
-        //return 1;
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/giveAllLearnrDisplay', true);
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.addEventListener('readystatechange', function(){
+            if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
+                var item = xhr.responseText;
+                var SuccessMSG = JSON.parse(item);
+                if (SuccessMSG.SuccessNum === 0){
+                    $scope.jsLearnRArray = SuccessMSG.TheDisplayLearnrs;
+                    console.log("Here is our jsLearnRArray: " + JSON.stringify($scope.jsLearnRArray));
+                    //Test add the p
+                    for (var a = 0; a < $scope.jsLearnRArray.length; a++){
+                        var resultLearnrHolder = document.createElement("p");
+                        resultLearnrHolder.setAttribute("id", "testp" + a.toString());
+                        resultLearnrHolder.setAttribute("class", "noot");
+                        resultLearnrHolder.setAttribute("name", "testp" + n.toString());
+                        resultLearnrHolder.innerHTML = String($scope.jsLearnRArray);
+                        
+                    }
+                } else {
+                    console.log("Failed to reach out to giveAllLearnrDisplay");
+                }
+            }
+        });
+        xhr.send("testsend");
     };
-    $scope.learnRSet();
+    //$scope.learnRSet();
     //Set functions for learnrInform
     $scope.informAdder = function(value) {
         console.log("Trying to add informAdder");
@@ -93,157 +113,5 @@ app.factory('addLearnerService', function(){
 
 //Javascript stuff to call Angular and vice versa
 window.addEventListener('DOMContentLoaded', function(){
-    //getLearnrsAjax(); //Get all Learnsrs loaded to Angular
+    
 });
-
-function getLearnrsAjax(){
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', '/giveAllLearnrDisplay', true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.addEventListener('readystatechange', function(){
-        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200){
-            var item = xhr.responseText;
-            var SuccessMSG = JSON.parse(item);
-            if (SuccessMSG.SuccessNum === 0){
-                return item;
-            } else {
-                console.log("Failed to reach out to giveAllLearnrDisplay");
-                return item;
-            }
-        }
-    });
-    xhr.send("testsend");
-}
-
-/* Send the created LearnRInform into the learnrAssemble Array 
-with LearnrInforms inside it */
-function sendLearnRInform(){
-    //Add this learnInform to the inform array in Angular
-    var scope = angular.element(document.getElementById('theMainBody')).scope();
-    var returnvalue = scope.informAdder(learnrInforms);
-    console.log("Here is the return value: " + JSON.stringify(returnvalue));
-    //Reset LearnrInform
-    var newLearnrInform = {
-        ID: 0,
-        Name: "",
-        LearnRID: 0,
-        LearnRName: "",
-        Order: 0,
-        TheInfo: "",
-        ShouldWait: true,
-        WaitTime: 0,
-        DateCreated: "",
-        DateUpdated: ""
-    };
-    learnrInforms = newLearnrInform;
-}
-
-/* Add the learnr to our array once it's assembled */
-function sendLearnR(){
-    var scope = angular.element(document.getElementById('theMainBody')).scope();
-    scope.learnrAdder(learnrAssemble);
-    //Reset Learnr
-    var newLearnr = {
-        ID: 0,
-        InfoID: 0,
-        OrgID: 0,
-        Name: "",
-        Tags: [],
-        Description: [],
-        PhoneNums: [],
-        LearnrInforms: [],
-        Active: true,
-        DateCreated: "",
-        DateUpdated: ""
-    };
-    learnrAssemble = newLearnr;
-}
-
-/* DEBUG: GET THE LEARNRS FROM JAVASCRIPT */
-function getLearnrs(){
-    var allLearnrs = [];
-
-    var scope = angular.element(document.getElementById('theMainBody')).scope();
-    allLearnrs = scope.giveLearnrs();
-    console.log("Here is the return value: " + JSON.stringify(allLearnrs));
-}
-
-/* Add LearnR values */
-function setid(thevalue){
-    learnrAssemble.ID = Number(thevalue);
-}
-
-function setinfoid(thevalue){
-    learnrAssemble.InfoID = Number(thevalue);
-}
-
-function setorgid(thevalue){
-    learnrAssemble.OrgID = Number(thevalue);
-}
-
-function setlearnrname(thevalue){
-    learnrAssemble.Name = String(thevalue);
-}
-
-function setlearnractive(thevalue){
-    learnrAssemble.Active = Boolean(thevalue);
-}
-
-function setlearnrdatecreated(thevalue){
-    learnrAssemble.DateCreated = String(thevalue);
-}
-
-function setlearnrupdated(thevalue){
-    learnrAssemble.DateUpdated = String(thevalue);
-}
-
-function settags(thevalue){
-    learnrAssemble.Tags = String(thevalue);
-}
-
-function setdescription(thevalue){
-    learnrAssemble.Description = String(thevalue);
-}
-
-function setlearnrphonenums(thevalue){
-    learnrAssemble.PhoneNums = String(thevalue);
-}
-
-
-/* Add LearnRInform values */
-
-function sendlearnrinformid(thevalue){
-    learnrInforms.ID = Number(thevalue);
-}
-
-function setlearnrinformname(thevalue){
-    learnrInforms.Name = String(thevalue);
-}
-
-function setlearnrnameinform(thevalue){
-    learnrInforms.LearnRName = String(thevalue);
-}
-
-function setlearnrinformorder(thevalue){
-    learnrInforms.Order = Number(thevalue);
-}
-
-function setlearnrinfo(thevalue){
-    learnrInforms.TheInfo = String(thevalue);
-}
-
-function setshouldwaitinform(thevalue){
-    learnrInforms.ShouldWait = Boolean(thevalue);
-}
-
-function setwaittimeinform(thevalue){
-    learnrInforms.WaitTime = Number(thevalue);
-}
-
-function setdatecreatedinform(thevalue){
-    learnrInforms.DateCreated = String(thevalue);
-}
-
-function setdateupdatedinform(thevalue){
-    learnrInforms.DateUpdated = String(thevalue);
-}
