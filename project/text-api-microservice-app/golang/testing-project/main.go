de1d4e5176170974f72bf495cd649a85042236b2
AmericanWonton <jbkeller0303@gmail.com>
2021-06-29 10:42:51 -0500

    Added crud changes after adding environment variables. Going to test them in docker.

--- project/text-api-microservice-app/golang/testing-project/main.go
+++ project/text-api-microservice-app/golang/testing-project/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"bufio"
-	b64 "encoding/base64"
 	"fmt"
 	"log"
 	"math/rand"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 )
 
@@ -39,8 +37,6 @@ func init() {
 	UserSessionActiveMap = make(map[int]UserSession) //Make Map not crazy
 	UserSessPhoneMap = make(map[string]int)
 	StopText = make(map[string]string)
-	//Initialize Mongo Creds
-	getCredsMongo()
 	//Initialize our bad phrases
 	getbadWords()
 	//Initialize our Twilio Cresd
@@ -52,61 +48,8 @@ func init() {
 func main() {
 	rand.Seed(time.Now().UTC().UnixNano()) //Randomly Seed
 
-	//Mongo Connect
-	mongoClient = connectDB()
-	defer mongoClient.Disconnect(theContext) //Disconnect in 10 seconds if you can't connect
 	//Handle our incoming web requests
 	handleRequests()
-}
-
-//Get mongo creds
-func getCredsMongo() {
-	file, err := os.Open("security/mongocreds.txt")
-
-	if err != nil {
-		fmt.Printf("Trouble opening file for Mongo Credentials: %v\n", err.Error())
-		logWriter("Trouble opening mongo credentials: " + err.Error())
-	}
-
-	scanner := bufio.NewScanner(file)
-
-	scanner.Split(bufio.ScanLines)
-	var text []string
-	var bigMongoString64 string
-	for scanner.Scan() {
-		text = append(text, scanner.Text())
-	}
-
-	file.Close()
-
-	for x := 0; x < len(text); x++ {
-		bigMongoString64 = bigMongoString64 + text[x]
-	}
-
-	sDecUsername, _ := b64.StdEncoding.DecodeString(text[1])
-	sDecPWord, _ := b64.StdEncoding.DecodeString(text[2])
-	sDB, _ := b64.StdEncoding.DecodeString(text[3])
-
-	theUsername := string(sDecUsername)
-	theUsername = strings.Replace(theUsername, "\n", "", 1)
-	thePWord := string(sDecPWord)
-	thePWord = strings.Replace(thePWord, "\n", "", 1)
-	theDB := string(sDB)
-	theDB = strings.Replace(theDB, "\n", "", 1)
-
-	mongoURI = makeMongoString(theUsername, thePWord, theDB, text[0])
-}
-
-/* This makes the mongo string from our base64 encoded password, username,
-and database name */
-func makeMongoString(theUsername string, thePword string, theDB string, mongoString string) string {
-	finishedMongoString := mongoString
-
-	finishedMongoString = strings.Replace(finishedMongoString, "<USER_NAME>", theUsername, 1)
-	finishedMongoString = strings.Replace(finishedMongoString, "<PASSWORD>", thePword, 1)
-	finishedMongoString = strings.Replace(finishedMongoString, "<THE_DB>", theDB, 1)
-
-	return finishedMongoString
 }
 
 //This gets the slur words we check against in our username and
--- project/text-api-microservice-app/golang/testing-project/mongoCrudOperations.go
+++ project/text-api-microservice-app/golang/testing-project/mongoCrudOperations.go
@@ -10,10 +10,6 @@ import (
 	"strconv"
 	"strings"
 	"time"
-
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 //Get User
@@ -35,12 +31,6 @@ const ADDLEARNRSESSIONSURL string = "http://localhost:4000/addLearnRSession"
 const READLEARNRSESSIONSURL string = "http://localhost:4000/getLearnRSession"
 const UPDATELEARNRSESSIONSURL string = "http://localhost:4000/updateLearnRSession"
 const DELETELEARNRSESSIONSURL string = "http://localhost:4000/deleteLearnRSession"
-
-//Mongo DB Declarations
-var mongoClient *mongo.Client
-
-var theContext context.Context
-var mongoURI string //Connection string loaded
 
 /* Variable definitions for User/Learnr */
 type User struct {
@@ -123,30 +113,6 @@ type LearnRInforms struct {
 	WaitTime    int    `json:"WaitTime"`   //How much time should User be given to read this text?
 	DateCreated string `json:"DateCreated"`
 	DateUpdated string `json:"DateUpdated"`
-}
-
-//This gets the client to connect to our DB
-func connectDB() *mongo.Client {
-	//Setup Mongo connection to Atlas Cluster
-	theClient, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
-	if err != nil {
-		fmt.Printf("Errored getting mongo client: %v\n", err)
-		log.Fatal(err)
-	}
-	theContext, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	err = theClient.Connect(theContext)
-	if err != nil {
-		fmt.Printf("Errored getting mongo client context: %v\n", err)
-		log.Fatal(err)
-	}
-	//Double check to see if we've connected to the database
-	err = theClient.Ping(theContext, readpref.Primary())
-	if err != nil {
-		fmt.Printf("Errored pinging MongoDB: %v\n", err)
-		log.Fatal(err)
-	}
-
-	return theClient
 }
 
 /* Calls our MongoDB Microservice to add a learnrsession to our DB */
